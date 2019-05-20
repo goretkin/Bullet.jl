2e16aae21f9cbb7cca8ab8e53ad6ab7f7e87f423
Gustavo Goretkin <gustavo.goretkin@gmail.com>
2019-05-20 03:55:07 -0400

    Test batch ray cast

--- src/wrap.jl
+++ src/wrap.jl
@@ -334,13 +334,32 @@ function raycast(sm, ray_from, ray_to)
   Raw.b3GetRaycastInformation(sm, rci_ref)
   rci = rci_ref[]
 
-  @assert rci.m_numRayHits <= 1
-  if rci.m_numRayHits == 0
-    #return nothing
-    error("even if ray doesn't hit, expected a hit object")
-  end
+  @assert rci.m_numRayHits == 1
   ray_hit = unsafe_load(rci.m_rayHits)
   return ray_hit
+end
+
+function raycast_batch(sm, rays_from::Array{Cdouble, 2}, rays_to::Array{Cdouble, 2},)
+  # must ensure memory layout matches Bullet's
+  @assert size(rays_from, 1) == 3
+  @assert size(rays_to, 1) == 3
+  @assert size(rays_from, 2) == size(rays_to, 2)
+  N = size(rays_from, 2)
+  @assert N <= Raw.MAX_RAY_INTERSECTION_BATCH_SIZE
+
+  command_handle = Raw.b3CreateRaycastBatchCommandInit(sm)
+  #Raw.b3RaycastBatchSetNumThreads(command_handle, 1)
+
+  Raw.b3RaycastBatchAddRays(sm, command_handle, rays_from, rays_to, N)
+
+  status_handle = submit_client_command_and_wait_status_checked(sm, command_handle; checked_status=Raw.CMD_REQUEST_RAY_CAST_INTERSECTIONS_COMPLETED)
+
+  rci_ref = Ref{Raw.b3RaycastInformation}()
+  Raw.b3GetRaycastInformation(sm, rci_ref)
+
+  rci = rci_ref[]
+  @assert rci.m_numRayHits == N
+  rci
 end
 
 function user_debug_draw_add_line(sm, from, to, color, line_width, life_time)
--- test/raycast.jl
+++ test/raycast.jl
@@ -12,9 +12,10 @@ sm = Bullet.connect(
 box_id = Bullet.create_body_box(sm, [1.0, 0.1, 1.0], TX(0.5,0.5,1.0,0,0,0), ColorTypes.RGBA(1,0,0,0.1))
 
 eye = [0, 0, 0.5]
-targets = [[1.0, y, z] for y = -2:0.2:2, z = -2:0.2:2]
+targets = [[1.0, y, z] for y = -2:0.3:2, z = -2:0.3:2]
 
-hits = [Bullet.raycast(sm, eye, target) for target in targets]
+single_time = @elapsed (hits = [Bullet.raycast(sm, eye, target) for target in targets])
+@show single_time
 
 if Bullet.connection_state.kind == :gui
   for raycast_i in eachindex(targets)
@@ -30,8 +31,24 @@ end
 hit_image = [hit.m_hitObjectUniqueId for hit in hits]
 
 @testset "hit image" begin
-  @test all(hit_image[1:10, 1:10] .== -1)
-  @test all(hit_image[14:end, 10:end] .== 0)
-  @test all(hit_image[1:10, 10:end] .== -1)
+  @test all(hit_image[1:8, :] .== -1)
+  @test all(hit_image[10:end, 7:end] .== 0)
 end
+
+@testset "batch raycast" begin
+  targets_flat = hcat(targets[:]...)
+  sources_flat = hcat((eye for _ in 1:size(targets_flat, 2))...)
+  batch_time_first = @elapsed (raycast_info = Bullet.raycast_batch(sm, sources_flat, targets_flat))
+  @show batch_time_first # includes compilation time.
+  batch_time = @elapsed (raycast_info = Bullet.raycast_batch(sm, sources_flat, targets_flat))
+  @show batch_time
+
+  lin_idxs = LinearIndices(targets)
+  hits_batch = [unsafe_load(raycast_info.m_rayHits, lin_idxs[cart_idx]) for cart_idx in CartesianIndices(targets)]
+
+  hit_batch_image = [hit.m_hitObjectUniqueId for hit in hits_batch]
+
+  @test all(hit_batch_image .== hit_image)
+end
+
 Bullet.disconnect()
